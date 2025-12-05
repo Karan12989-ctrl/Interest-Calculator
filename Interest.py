27ccb6637e270b164ca657e3cf36d0b30d5affe0
Karan Prajapati <pokemonfan991122@gmail.com>
2025-12-05 19:15:03 +0530

    Interest calculator python working is processing.

--- Interest.py
+++ Interest.py
@@ -1,2 +1,27 @@
 def s_interest(p,r,t):
-    return 
+    return (p*r*t)/100
+
+def c_interest(p,r,t):
+    amount=p*(1+r/100)**t
+    return amount-p
+
+print("-----INTEREST CALCULATOR-----")
+print("1. Simple Interest")
+print("2. Compound Interest")
+
+choice=int(input("Enter your choice for Interest (1 or 2): "))
+
+principal=float(input("Enter your Principal Amount: "))
+rate=float(input("Enter your Rate of Interest(%): "))
+time=float(input("Enter your Rate of Year: "))
+
+if choice==1:
+    si=s_interest(principal,rate,time)
+    print(f"Simple Interest = {si:.2f}")
+    print(f"Total Amount = {principal + si:.2f}")
+elif choice==2:
+    ci=c_interest(principal,rate,time)
+    print(f"Compund Interest = {ci:.2f}")
+    print(f"Total Amount = {principal + ci:.2f}")
+else:
+    print("Choice (1 & 2) is Invalid, Try Again")
